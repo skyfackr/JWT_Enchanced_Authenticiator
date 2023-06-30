400793343ea62296ba47a9ee31621cdd1be77bf7
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2023-06-30 16:28:23 +0800

    rollback some test code

--- src/main/java/wang/skycloud/JWTEA/Main.java
+++ src/main/java/wang/skycloud/JWTEA/Main.java
@@ -3,9 +3,7 @@ package wang.skycloud.JWTEA;
 // 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
 // 然后按 Enter 键。现在，您可以在代码中看到空格字符。
 public class Main {
-
-    private int shitNumber;
-    public static void main_function(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("前排兜售小main包~");
 
