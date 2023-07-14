d184d9ce2383392a2ed260604b49cc56db0d1c5a
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2023-07-14 16:00:31 +0800

    add javadoc

--- Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPackager.java
+++ Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPackager.java
@@ -9,12 +9,26 @@ import wang.skycloud.JWTEA.Client.SignedPayload.SignedPayload;
  * @date 2023/07/14
  */
 public class PreRequestPackager {
+    /**
+     * 有效载荷
+     */
     private final PreRequestPayload payload;
 
+    /**
+     * 预请求打包器
+     *
+     * @param payload 有效载荷
+     */
     private PreRequestPackager(PreRequestPayload payload) {
         this.payload = payload;
     }
 
+    /**
+     * 将{@link PreRequestPayload}加密以及签名，并封装为{@link SignedPayload}
+     *
+     * @param payload 已经完成所有数据填充的payload 未完成会导致报错
+     * @return {@link SignedPayload}
+     */
     public static SignedPayload packPreRequest(PreRequestPayload payload) {
         PreRequestPackager packager=new PreRequestPackager(payload);
         //TODO: packagePreRequest not implemented created at 2023/7/14 15:35
