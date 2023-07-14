be673d2422e657b542c1e819419db41a218c1b99
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2023-07-14 15:41:27 +0800

    write javadoc to define two class
    and also add some static code to packager

--- Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestMaker.java
+++ Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestMaker.java
@@ -1,4 +1,10 @@
 package wang.skycloud.JWTEA.Client.PreRequest;
 
+/**
+ * 预请求制造器
+ *
+ * @author sky
+ * @date 2023/07/14
+ */
 public class PreRequestMaker {
 }
--- Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPackager.java
+++ Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPackager.java
@@ -1,4 +1,23 @@
 package wang.skycloud.JWTEA.Client.PreRequest;
 
+import wang.skycloud.JWTEA.Client.SignedPayload.SignedPayload;
+
+/**
+ * 预请求打包器 打包为{@link SignedPayload}
+ *
+ * @author sky
+ * @date 2023/07/14
+ */
 public class PreRequestPackager {
+    private final PreRequestPayload payload;
+
+    private PreRequestPackager(PreRequestPayload payload) {
+        this.payload = payload;
+    }
+
+    public static SignedPayload packPreRequest(PreRequestPayload payload) {
+        PreRequestPackager packager=new PreRequestPackager(payload);
+        //TODO: packagePreRequest not implemented created at 2023/7/14 15:35
+        throw new RuntimeException("packagePreRequest not implemented");
+    }
 }
