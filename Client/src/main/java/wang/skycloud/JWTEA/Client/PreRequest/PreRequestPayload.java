f094dae9c952371c49e4eae01916156094b2cfef
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2023-07-14 16:58:44 +0800

    Update PreRequestPayload.java
    implement todo

--- Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPayload.java
+++ Client/src/main/java/wang/skycloud/JWTEA/Client/PreRequest/PreRequestPayload.java
@@ -1,9 +1,11 @@
 package wang.skycloud.JWTEA.Client.PreRequest;
 
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 import wang.skycloud.JWTEA.Client.Enum.EncryptAlgorithms;
 import wang.skycloud.JWTEA.Client.Enum.SignatureAlgorithms;
 import wang.skycloud.JWTEA.Client.PreRequest.Exceptions.PayloadContainsNullException;
+import wang.skycloud.JWTEA.Client.SignedPayload.SignedPayload;
 
 import java.util.Date;
 
@@ -59,21 +61,22 @@ public class PreRequestPayload {
     /**
      * 验证有效载荷
      * 验证payload是否为空
+     * 这是一个assert方法，验证失败会抛出异常，通过则会返回自身
      *
      * @throws PayloadContainsNullException 有效负载包含空
      */
-    void verifyPayload() throws PayloadContainsNullException
+    public PreRequestPayload verifyPayload() throws PayloadContainsNullException
     {
         if (!isPayloadFulfilled()) throw new PayloadContainsNullException(this);
+        return this;
     }
 
     /**
-     * 设置当前时间戳
+     * 设置时间戳为调用时间
      */
-    void setCurrentTimeAsStamp()
+    public void setCurrentTimeAsStamp()
     {
-        //TODO: setCurrentTimeAsStamp not implemented created at 2023/7/14 15:53
-        throw new RuntimeException("setCurrentTimeAsStamp not implemented");
+        this.setTimestamp(new Date());
     }
 
     /**
@@ -81,9 +84,39 @@ public class PreRequestPayload {
      *
      * @return boolean
      */
+    @SuppressWarnings("ConstantValue")
     public boolean isPayloadFulfilled()
     {
-        //TODO: isPayloadFulfilled not implemented created at 2023/7/14 16:16
-        throw new RuntimeException("isPayloadFulfilled not implemented");
+        //check non null
+        if (
+                getVersion()==null||
+                        getId()==null||
+                        getServerRSAId()==null||
+                        getServerRSA()==null||
+                        getIdAlgo()==null||
+                        getSalt()==0||
+                        getTimestamp()==null||
+                        getSignAlgo()==null
+        ) return false;
+        //check string non empty and non blank
+        if (
+
+                getVersion().isBlank()||
+                        getServerRSAId().isBlank() ||
+                        getServerRSA().isBlank()
+        ) return false;
+        //check id non null
+        if (
+                id.getClientRSAPublicKey() == null ||
+                        id.getCredential() == null ||
+                        id.getClientRSAPrivateKey() == null
+        ) return false;
+        //check id string non empty and non blank
+        if (
+                id.getClientRSAPublicKey().isBlank() ||
+                        id.getCredential().isBlank() ||
+                        id.getClientRSAPrivateKey().isBlank()
+        ) return false;
+        return true;
     }
 }
